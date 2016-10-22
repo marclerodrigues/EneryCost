5bc92f42ea339346891e0d9802496a7d851e8d84
Marcle Rodrigues <maarclee@gmail.com>
2016-10-21 22:48:07 -0300

    Add cost method to EnergyCost

--- lib/energy_cost.ex
+++ lib/energy_cost.ex
@@ -1,2 +1,6 @@
 defmodule EnergyCost do
+  def cost(kW_month, kW_price), do: kW_month * kW_price
+  def cost(kW_month, kW_price, days) do
+    Float.round((kW_month * kW_price * days / 30), 2)
+  end
 end
--- test/energy_cost_test.exs
+++ test/energy_cost_test.exs
@@ -5,4 +5,12 @@ defmodule EnergyCostTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "returns the correct result provided two params" do
+    assert EnergyCost.cost(1, 10) == 10
+  end
+
+  test "returns the correct result provided three params" do
+    assert EnergyCost.cost(1, 30, 3) == 3
+  end
 end
