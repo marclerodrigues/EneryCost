2f9940e9c2b3839ac9b42fa3bb21cac1f2702335
Marcle Rodrigues <maarclee@gmail.com>
2016-10-22 23:17:48 -0300

    Refactor EnergyCost module

--- lib/energy_cost.ex
+++ lib/energy_cost.ex
@@ -1,4 +1,6 @@
 defmodule EnergyCost do
+  def cost(0, kW_price), do: 0
+  def cost(1, kW_price), do: kW_price
   def cost(kW_month, kW_price), do: kW_month * kW_price
   def cost(kW_month, kW_price, days) do
     Float.round((kW_month * kW_price * days / 30), 2)
--- test/energy_cost_test.exs
+++ test/energy_cost_test.exs
@@ -6,6 +6,14 @@ defmodule EnergyCostTest do
     assert 1 + 1 == 2
   end
 
+  test "returns zero when amount worn it zero" do
+    assert EnergyCost.cost(0, 10) == 0
+  end
+
+  test "returns the price when amount worn is 1" do
+    assert EnergyCost.cost(1, 2) == 2
+  end
+
   test "returns the correct result provided two params" do
     assert EnergyCost.cost(1, 10) == 10
   end
